57f1ad468a46f3e9f52ae6587485a77e0bf5bd6d
gn10g <8gn24gn25@gmail.com>
2025-04-02 14:30:01 +0900

    enable:uart it

--- Firmware/GN10Mainboard/Core/Inc/stm32g4xx_it.h
+++ Firmware/GN10Mainboard/Core/Inc/stm32g4xx_it.h
@@ -55,6 +55,8 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
 void FDCAN2_IT0_IRQHandler(void);
 void FDCAN3_IT0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
